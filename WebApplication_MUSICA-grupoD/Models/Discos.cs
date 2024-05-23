84aad2dde29bdbceec9509edf32f3f7f55a9d925
Diknakort <diknakort@gmail.com>
2024-05-23 14:45:01 +0200

    actualizacion 01

--- WebApplication_MUSICA-grupoD/Models/Discos.cs
+++ WebApplication_MUSICA-grupoD/Models/Discos.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication_MUSICA_grupoD.Models;
 
@@ -8,11 +10,11 @@ public partial class Discos
     public int Id { get; set; }
 
     public string? Nombre { get; set; }
-
+    [DataType(DataType.Date)]
     public DateOnly? Fecha { get; set; }
-
+    [DisplayName("Genero musical")]
     public int? GeneroID { get; set; }
-
+    [DisplayName("Grupo")]
     public int? GruposId { get; set; }
 
     public virtual ICollection<Canciones> Canciones { get; set; } = new List<Canciones>();
